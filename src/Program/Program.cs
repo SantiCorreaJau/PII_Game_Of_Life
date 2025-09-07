cda6dd18c828caddd4669738e18a7b6ba6f5b533
SantiCorreaJau <santicorreajau@gmail.com>
2025-09-07 18:40:41 -0300

    actualizo Program.cs
    cambie la ruta a board.txt

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,7 +10,7 @@ public class Program
         ConsoleRenderer renderer = new ConsoleRenderer(); // Crea el que muestra el estado actual en la pantalla
         GameLoop loop = new GameLoop(renderer, engine); // Crea la clase que dirije el juego
         
-        string path = "ruta_del_archivo.txt"; // Ruta del archivo
+        string path = "board.txt"; // Ruta del archivo
         bool[,] board = reader.Read(path); // Carga el archivo de texto e inicializa el primer tablero
 
         loop.Run(board, 300); // Comienza el bucle con delay 300ms
